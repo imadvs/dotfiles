bf50f8606ceb40342fd5508d90dd665e7f41a0b8
imadvs <imavs00@gmail.com>
2026-01-02 20:31:01 +0100

    Update Fri Jan  2 08:31:01 PM +01 2026

--- hypr/.config/hypr/scripts/kill_workspace.sh
+++ hypr/.config/hypr/scripts/kill_workspace.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Get the ID of the currently active workspace
+
+# 1. Get the current active workspace ID
 ACTIVE_WS=$(hyprctl activeworkspace -j | jq '.id')
 
-# Get PIDs of all windows on that workspace and kill them
-hyprctl clients -j | jq ".[] | select(.workspace.id == $ACTIVE_WS) | .pid" | xargs kill
+# 2. Get the 'address' of every window on that workspace
+# 3. Tell Hyprland to close each of those addresses specifically
+hyprctl clients -j | jq -r ".[] | select(.workspace.id == $ACTIVE_WS) | .address" | while read -r addr; do
+    hyprctl dispatch closewindow address:"$addr"
+done
